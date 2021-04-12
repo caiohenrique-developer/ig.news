03ac8bf79c4326d209f6e1e9a61c44567ede36e6
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-12 17:33:29 -0300

    :art: [ STYLE ]: Add global styles of the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
 
+import "../styles/globals.sass";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head';
 
-import styles from '../styles/pages/home.module.sass';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Ig.News | 👋 Hey, welcome</title>
       </Head>
-      
-      <h1 className={styles.title}>Index <span>page</span></h1>
+
+      <h1>Index <span>page</span></h1>
     </>
   )
 }
