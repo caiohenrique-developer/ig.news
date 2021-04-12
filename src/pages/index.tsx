1ec77253482be0c00079e5390af2c4103ee182c0
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-12 17:05:14 -0300

    :recycle: [ REFACTOR ]: Add tag title per pages, like home page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
 import styles from '../styles/pages/home.module.sass';
 
 export default function Home() {
-  return <h1 className={styles.title}>Index <span>page</span></h1>
+  return (
+    <>
+      <Head>
+        <title>Ig.News | 👋 Hey, welcome</title>
+      </Head>
+      
+      <h1 className={styles.title}>Index <span>page</span></h1>
+    </>
+  )
 }
