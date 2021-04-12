9880de99472d5dee9df72b91e2bed73c4f47e492
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-12 15:44:12 -0300

    :hammer: [ CHORE ]: Add Sass to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import style from '../styles/pages/home.module.sass';
+
 export default function Home() {
-  return <h1>Index page</h1>
+  return <h1 className={style.title}>Index <span>page</span></h1>
 }
