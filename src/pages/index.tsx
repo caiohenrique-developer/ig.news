0a05c678aa58e57adc8f3d9e8cfa0d911ea9e494
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-12 16:25:59 -0300

    :hammer: [ CHORE ]: Create _document file and add Roboto font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import style from '../styles/pages/home.module.sass';
+import styles from '../styles/pages/home.module.sass';
 
 export default function Home() {
-  return <h1 className={style.title}>Index <span>page</span></h1>
+  return <h1 className={styles.title}>Index <span>page</span></h1>
 }
